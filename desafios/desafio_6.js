de3201f1354049e844396a5a01da77b09ed51028
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-04-07 19:25:23 -0300

    desafio 6

---
+++ desafios/desafio_6.js
@@ -0,0 +1,36 @@
+// use aggregations;
+db.movies.aggregate([
+  {
+    $match:
+    {
+      awards: { $exists: true },
+      awards: { $regex: /Won [1-20] Oscar/ }
+    }
+  },
+  {
+    $addFields:
+    {
+      rating: '$imdb.rating'
+    }
+  },
+  {
+    $group:
+    {
+      _id: null,
+      maior_rating: { $max: '$rating' },
+      menor_rating: { $min: '$rating' },
+      media_rating: { $avg: '$rating' },
+      desvio_padrao: { $stdDevSamp: '$rating' }
+    }
+  },
+  {
+    $project: 
+    {
+      _id: 0,
+      maior_rating: '$maior_rating',
+      menor_rating: '$menor_rating',
+      media_rating: '$media_rating',
+      desvio_padrao: '$desvio_padrao'
+    }
+  }
+]).pretty();
