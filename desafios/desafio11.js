091ff02e6b01f4bb577e48fb30eddec884edef02
antoniopaolillo <antonio_francisco_p@live.com>
2020-04-08 17:40:07 -0300

    arrumando o 6 e upando o 11

--- desafios/desafio11.js
+++ desafios/desafio11.js
@@ -1,18 +1,22 @@
 // use aggregations;
 db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
   {
     $lookup: {
       from: "air_routes",
-      localField: "airline.name",
-      foreignField: "airlines",
-      as: "rota"
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "rotas"
     }
   },
-  {$limit: 1}
+  { $unwind: "$rotas" },
+  { $match: { "rotas.airplane": { $in: ["747", "380"] } } },
+  {
+    $group: {
+      _id: "$rotas.airline.name",
+      totalRotas: { $sum: 1 }
+    }
+  },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 }
 ]);
-
-// {
-//   $match: {
-//     airplane: { $in: ["747", "380"] }
-//   }
-// }
