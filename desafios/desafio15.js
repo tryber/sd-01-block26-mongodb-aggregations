47f9ca67e275646c2865bb6aaa61cc43e4a5bd0c
antoniopaolillo <antonio_francisco_p@live.com>
2020-04-08 19:43:28 -0300

    desafios 14 e 15

---
+++ desafios/desafio15.js
@@ -0,0 +1,26 @@
+// use aggregations;
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: {
+        $exists: true,
+        $ne: ""
+      }
+    }
+  },
+  {
+    $set: {
+      idade: {
+        $floor: {
+          $subtract: [{ $year: "$$NOW" }, { $toInt: "$birthYear" }]
+        }
+      }
+    }
+  },
+  {
+    $bucketAuto: {
+      groupBy: "$idade",
+      buckets: 5,
+    }
+  },
+]);
