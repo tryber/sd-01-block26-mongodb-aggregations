5787a746b52ab1b81f88763a551442c4304ff937
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-04-08 14:35:14 -0300

    code climate

--- challengeMongoDB/desafio9.js
+++ challengeMongoDB/desafio9.js
@@ -1,20 +1,20 @@
-db.movies.aggregate([
-  { $match: { 'imdb.rating': { $gte: 0 }, metacritic: { $gte: 0 } } },
-  {
-    $facet: {
-      top10IMDB: [
-        { $project: { titulo: '$title', notaIMDB: '$imdb.rating' } },
-        { $sort: { notaIMDB: -1 } },
-        { $limit: 10 }
-      ],
-      top10Metacritic: [
-        { $project: { titulo: '$title', notaMetacritic: '$metacritic' } },
-        { $sort: { notaMetacritic: -1 } },
-        { $limit: 10 }
-      ]
-    }
-  },
-]).pretty();
+// db.movies.aggregate([
+//   { $match: { 'imdb.rating': { $gte: 0 }, metacritic: { $gte: 0 } } },
+//   {
+//     $facet: {
+//       top10IMDB: [
+//         { $project: { titulo: '$title', notaIMDB: '$imdb.rating' } },
+//         { $sort: { notaIMDB: -1 } },
+//         { $limit: 10 }
+//       ],
+//       top10Metacritic: [
+//         { $project: { titulo: '$title', notaMetacritic: '$metacritic' } },
+//         { $sort: { notaMetacritic: -1 } },
+//         { $limit: 10 }
+//       ]
+//     }
+//   },
+// ]).pretty();
 
 // {$unwind: '$top10IMDB'},
 // {$unwind: '$top10Metacritic'},
