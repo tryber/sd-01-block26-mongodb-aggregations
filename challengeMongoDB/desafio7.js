b13b0121cfee6946fbc8d020da5c904d516590f4
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-04-08 01:42:32 -0300

    corrigindo e adicionando exercicios

---
+++ challengeMongoDB/desafio7.js
@@ -0,0 +1,19 @@
+db.movies.aggregate([
+  {
+    $match: { languages: 'English', cast: { $exists: true } }
+  },
+  { $unwind: '$cast' },
+  {
+    $group: { 
+      _id: '$cast',
+      numeroFilmes: { $sum: 1 }, 
+      mediaIMDB: { $avg: '$imdb.rating' }
+    }
+  },
+  {
+    $project: {
+      numeroFilmes: 1, mediaIMDB: {$trunc: ['$mediaIMDB', 1]}
+    }
+  },
+  {$sort: {numeroFilmes: -1}}
+]);
---
+++ challengeMongoDB/desafio9.js
@@ -0,0 +1,30 @@
+db.movies.aggregate([
+  { $match: { 'imdb.rating': { $gte: 0 }, metacritic: { $gte: 0 } } },
+  {
+    $facet: {
+      top10IMDB: [
+        { $project: { titulo: '$title', notaIMDB: '$imdb.rating' } },
+        { $sort: { notaIMDB: -1 } },
+        { $limit: 10 }
+      ],
+      top10Metacritic: [
+        { $project: { titulo: '$title', notaMetacritic: '$metacritic' } },
+        { $sort: { notaMetacritic: -1 } },
+        { $limit: 10 }
+      ]
+    }
+  },
+  // {
+  //   $lookup: {
+  //     from: 'movies',
+  //     localField: 'top10IMDB',
+  //     foreignField: 'top10Metacritic',
+  //     as: 'intersection'
+  //   }
+  // },
+]).pretty();
+
+// {$unwind: '$top10IMDB'},
+// {$unwind: '$top10Metacritic'},
+
+//incompleto
