41e0f590cdf432b67b4b9e42eea685f9df096109
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-04-08 21:36:43 -0300

    Parando no 13. O 12, sem channce por hoje

---
+++ desafios/desafio_11.js
@@ -0,0 +1,22 @@
+// use aggregations;
+db.air_alliances.aggregate([
+  { $unwind: '$airlines' },
+  {
+    $lookup: {
+      from: 'air_routes',
+      localField: 'airlines',
+      foreignField: 'airline.name',
+      as: 'rotas'
+    }
+  },
+  { $unwind: '$rotas' },
+  { $match: { 'rotas.airplane': { $in: ['747', '380'] } } },
+  {
+    $group: {
+      _id: '$rotas.airline.name',
+      totalRotas: { $sum: 1 }
+    }
+  },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 }
+]).pretty();
