3e3f29fe4ad72662bb2b75667a66aa5d54d3b4f1
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-04-13 17:06:28 -0300

    Desafio 15 com alguns errinhos

--- desafios/desafio_15.js
+++ desafios/desafio_15.js
@@ -1,25 +1,26 @@
 use aggregations;
-db.trips.aggregate([
-  {
-    $match: { birthYear: { $exists: true, $ne: '' } }
-  },
-  {
-    $addFields: {
-      idade: { $subtract: [{ $year: '$$NOW' }, { $toInt: '$birthYear' }] }
-    }
-  },
-  {
-    $bucketAuto: {
-      groupBy: '$idade',
-      buckets: 5,
-      output: {
-        count: { $sum: 1 }
+db.trips.aggregate(
+  [
+    {
+      $match: { birthYear: { $exists: true, $ne: '' } }
+    },
+    {
+      $addFields: {
+        idade: { $subtract: [{ $year: '$$NOW' }, { $toInt: '$birthYear' }] }
+      }
+    },
+    {
+      $bucketAuto: {
+        groupBy: '$idade',
+        buckets: 5,
+        output: {
+          count: { $sum: 1 }
+        }
       }
     }
+  ],
+  {
+    allowDiskUse: true,
+    cursor: {}
   }
-],
-{
-  allowDiskUse:true,
-  cursor:{}
- }).pretty();
- 
+).pretty();
