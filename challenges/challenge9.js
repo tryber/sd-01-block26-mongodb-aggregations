8525c655c675593d89c6f55e8304d10fd5b5910d
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-04-22 13:30:05 -0300

    exercicio 9

---
+++ challenges/challenge9.js
@@ -0,0 +1,24 @@
+use aggregations;
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 0 },
+      metacritic: { $gte: 0 }
+    }
+  },
+  {
+    $facet: {
+      top10IMDB: [{ $sort: { "imdb.rating": -1 } }, { $limit: 10 }],
+      top10Metacritic: [{ $sort: { metacritic: -1 } }, { $limit: 10 }]
+    }
+  },
+  {
+    $group: {
+      _id: { $setIntersection: ["$top10IMDB", "$top10Metacritic"] },
+      filmesEmComum: { $sum: 1 }
+    }
+  },
+  { 
+    $project: { _id: 0, filmesEmComum: 1 } 
+  }
+]);
