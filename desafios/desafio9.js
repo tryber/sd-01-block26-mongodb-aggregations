a7a98bece4cedd6551bb372b2243f342e2861a5f
Anderson Bolivar <55351352+andyboli@users.noreply.github.com>
2020-04-23 11:10:56 -0300

    Update desafio9.js

--- desafios/desafio9.js
+++ desafios/desafio9.js
@@ -3,70 +3,21 @@
 use aggregations;
 
 db.movies.aggregate([
+  { $match: { 'imdb.rating': { $gte: 0 }, metacritic: { $gte: 0 } } },
   {
-    $match: {
-      'imdb.rating': { $gt: 0 },
-      'metacritic': { $gt: 0 }
-    }
-  },
-  {
-    $sort: {
-      'imdb.rating': -1,
-    }
-  },
-  {
-    $limit: 10,
-  },
-  {
-    $group: {
-      '_id': null,
-      'top10IMDB': {
-        $push: {
-          '_id': '$_id',
-          'titulo': '$title',
-        }
-      }
-    }
-  },
-  {
-    $lookup: {
-      from: 'movies',
-      pipeline: [
-        {
-          $match: {
-            'imdb.rating': { $gt: 0 },
-            'metacritic': { $gt: 0 }
-          }
-        },
-        {
-          $sort: {
-            'metacritic': -1
-          },
-        },
-        {
-          $limit: 10,
-        },
-        {
-          $project: {
-            '_id': 1, 'titulo': '$title'
-          }
-        }
+    $facet: {
+      top10IMDB: [
+        { $sort: { 'imdb.rating': -1 } },
+        { $limit: 10 }
       ],
-      as: 'top10Metacritic'
+      top10Metacritic: [
+        { $sort: { 'metacritic': -1 } },
+        { $limit: 10 }
+      ]
     }
   },
   {
-    $project: {
-      '_id': 0,
-      'filmesEmComum': {
-        $size: {
-          $ifNull: [
-            { $setIntersection: ['$top10IMDB', '$top10Metacritic'] },
-            []
-          ]
-        }
-      }
-    }
-  },
+    $project: { filmesEmComum: { $size: { $setIntersection: ['$top10IMDB', '$top10Metacritic'] } } }
+  }
 ]);
 */
